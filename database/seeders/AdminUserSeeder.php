1cd2cfc29c4a2d839c668973b613c07c2a92fcd1
alexandreheigi82 <administrador.eig@gmail.com>
2024-12-01 17:59:31 -0300

    copia de segurança

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users_tabela')->insert([
+            'nome' => 'Admin',
+            'sobrenome' => 'User',
+            'email' => 'lunas.tours.tcc@gmail.com',
+            'senha' => Hash::make('Unialfa.01'),
+            'situacao' => true,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
